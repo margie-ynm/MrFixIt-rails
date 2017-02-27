27ce25780cd02f8ff2339216ec7506b42f86658d
Margie Nevarez <margie.ynm@gmail.com>
2017-02-26 17:44:38 -0800

    implement active mode to worker dashboard

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -24,16 +24,21 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-    if current_worker
-      if @job.update(pending: true, worker_id: current_worker.id)
+    if params[:mode] == 'pending' && current_worker
+      @job.update(pending: true, worker_id: current_worker.id)
         respond_to do |format|
           format.html {redirect_to job_path(@job)}
           format.js
         end
-      else
+    elsif params[:mode] == 'active' && current_worker
+      @job.update(pending: false, active: true, worker_id: current_worker.id)
+        respond_to do |format|
+          format.html {redirect_to job_path(@job)}
+          format.js
+        end
+      elsif
         render :show
         flash[:notice] = "Something went wrong!"
-      end
     else
       # We need to streamline this process better in the future! - Mr. Fix-It.
       flash[:notice] = 'You must have a worker account to claim a job. Register for one using the link in the navbar above.'
