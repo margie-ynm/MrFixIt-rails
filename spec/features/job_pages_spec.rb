5c2efae9bf83762d59bd8ecf0318d09cbd9ccea9
Margie Nevarez <margie.ynm@gmail.com>
2017-02-27 16:59:04 -0800

    pass spec for making a job active with ajax

--- spec/features/job_pages_spec.rb
+++ spec/features/job_pages_spec.rb
@@ -2,25 +2,41 @@ require "rails_helper"
 
   describe "jobs pages" do
     before(:each) do
-      @user = FactoryGirl.create(:worker)
+      @worker = FactoryGirl.create(:worker)
       visit new_worker_session_path
-      fill_in 'Email', :with => @user.email
-      fill_in 'Password', :with => @user.password
+      fill_in 'Email', :with => @worker.email
+      fill_in 'Password', :with => @worker.password
       click_button 'Log in'
     end
 
-    it "allows a user to add a job" do
+    it "allows a worker to add a job" do
       visit new_job_path
       fill_in :Title, :with => "job 1"
       fill_in :Description, :with => "description 1"
       click_button "Create Job"
       expect(page).to have_content("job 1")
     end
-    it "a user can mark a job as pending", js: true do
+    it "a worker can mark a job as pending", js: true do
       job = create(:job)
       visit jobs_path
       click_link "job 1"
       click_link "click here to claim it now"
       expect(page).to have_content("You've successfully claimed this job!")
+    end
+    it "a worker can mark a job as pending", js: true do
+      job = create(:job)
+      visit jobs_path
+      click_link "job 1"
+      click_link "click here to claim it now"
+      expect(page).to have_content("You've successfully claimed this job!")
+    end
+    it "a worker can mark a job as active", js: true do
+      job = create(:job)
+      visit jobs_path
+      click_link "job 1"
+      click_link "click here to claim it now"
+      visit worker_path(@worker)
+      click_on "Make this job active"
+      expect(page).to have_content("description 1")
     end
   end
